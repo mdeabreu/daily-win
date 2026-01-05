d2f84d62474a0d6a14f4fe7abe4bc01425211524
Matthew DeAbreu <Matthew.DeAbreu@gmail.com>
2026-01-04 16:49:37 -0800

    fix timezone

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@
 docker build . -t daily-wins
 docker stop daily-wins
 docker rm daily-wins
-docker run -d --restart always --name daily-wins -p 3322:3000 -v $(pwd)/data:/app/data daily-wins
+docker run -d --restart always --name daily-wins -e TZ=America/Vancouver -p 3322:3000 -v $(pwd)/data:/app/data daily-wins
